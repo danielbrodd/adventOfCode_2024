3d46dcf289a66a31cabb5ebff2e989e9333e4022
Daniel Brodd <daniel.brodd@malmo.se>
2024-12-09 16:22:26 +0100

    continue day 4

--- day4/main_part_one.js
+++ day4/main_part_one.js
@@ -6,3 +6,27 @@ const sampleData = parse(samplePath)
 const data = parse(path)
 
 console.log(sampleData)
+
+function Main(data) {
+    for (let row of data){
+        row.forEach((char, i) => {
+            if (char === 'x' || char === 'm'){
+                console.log(char, i)
+            }
+        })
+        }
+    }
+
+function horizontalForward(row){
+}
+
+function horizontalBackward(row){}
+function verticalDown(twoDArray){}
+function verticalUp(twoDArray){}
+function diagonaDownRight(twoDArray){}
+function diagonaDownLeft(twoDArray){}
+function diagonaUpRight(twoDArray){}
+function diagonaUpLeft(twoDArray){}
+
+
+Main(sampleData)
