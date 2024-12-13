0c3a7f11771f8c582e5b6fc28c2354e782c50015
Daniel Brodd <daniel.brodd@malmo.se>
2024-12-13 11:18:44 +0100

    day 6 part 1 done

--- day6/main.js
+++ day6/main.js
@@ -1,39 +1,57 @@
 const parse = require('./parse');
 const samplePath =  './sample.txt';
+const path = './data.txt'
 
 const sample = parse(samplePath);
+const data = parse(path);
 
 function traverseMap(map) {
 
-  let directions = [[1,0],[0,1],[-1,0],[0,-1]];
-  let dir = 0
-  let me = [null, null]
-  const w = map[0].length
-  const h = map.length
-  let visited = []
-  console.log('w: ', w, 'h: ',h)
-  for (let row = 0; row < map.length; row++) {
-    for (let col = 0; col < map[row].length; col++) {
-      if (map[row][col] === '^') {
-        me[0] = row;
-        me[1] = col;
-        visited.push([row,col])
-      }
+    // Find 'me'
+    let directions = [[-1,0],[0,1],[1,0],[0,-1]];
+    let dir = 0
+    let me = [null, null]
+    const w = map[0].length
+    const h = map.length
+    let visited = new Set()
+    for (let row = 0; row < map.length; row++) {
+        for (let col = 0; col < map[row].length; col++) {
+            if (map[row][col] === '^') {
+                me[0] = row;
+                me[1] = col;
+            }
+        }
     }
-  }
-  console.log(me)
-  while (true) {
+    
+    console.log('w ',w,'h ', h)
+    // start traversal
+    while (true) {
+        map[me[0]][me[1]] = 'X';
+        visited.add(me.toString());
+        // set next step to traversal direction (initial is up)
+        let [r2,c2] = me.map((num, i) => num + directions[dir][i]);
+        // out of bounds check
+        if ((r2 < 0 || r2 >= h) || (c2 < 0 || c2 >= w)) {
+            break;
+        }
+        let next = map[r2][c2];
+        // if next position is a . (free to go) or X (already visited) move in said direction
+        // update map visually, add position to visited set, set me to new position
+        if ( next === '.' || next === 'X') {
+            map[r2][c2] = '^';
+            me = [r2,c2];
+        } else if (next === '#'){ 
+            // if there is an obstacle
+            dir = (dir + 1) % 4;
+            
+        };
+    }
+    const rowJoiner = process.platform === 'win32' ? '\r\n' : '\n';
 
-    let [r2,c2] = me.map((num, i) => num + directions[dir][i])
-    if (map[r2][c2] === '.') {
-      map[me[0]][me[1]] = 'X'
-      me = [r2,c2]
-
-    }
-    if((me.c < 0 || me.c > h) || (me.r < 0 || me.r > w)) {
-      break;
-    }
-  }
+    let txtMap = map.map(row => row.join('')).join(rowJoiner)
+    console.log(txtMap)
+    console.log(`traversed ${visited.size} unique positions`)
 }
 
 traverseMap(sample)
+traverseMap(data)
--- day6/parse.js
+++ day6/parse.js
@@ -1,14 +1,20 @@
 const fs = require('fs')
 
 function parse (path) {
-  const txt = fs.readFileSync(path, 'utf8')
-  const lines = txt.split('\n')
-  let res = [] 
-  lines.forEach(line => {
-    res.push(line.split(''))
-  })
-  res.pop()
-  return res;
+    const txt = fs.readFileSync(path, 'utf8')
+    let lines;
+    if (process.platform === 'win32') {
+        lines = txt.split('\r\n')
+    }
+    else {
+        lines = txt.split('\n')
+    }
+    let res = [] 
+    lines.forEach(line => {
+        res.push(line.split(''))
+    })
+    res.pop()
+    return res;
 }
 
 module.exports = parse;
