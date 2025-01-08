e0b50119f593eb208215e7a3f73f72c9b6c391c0
Daniel Brodd <daniel.brodd@malmo.se>
2025-01-08 16:27:16 +0100

    day9 part 2 done
    by cheating

--- day9/main_part_two.js
+++ day9/main_part_two.js
@@ -8,7 +8,8 @@ const data = parse(path)
 
 function main(array) {
     console.time('main')
-    let data = shiftMemory(transformData(array))
+    let data = transformData(array)
+    data = shiftMemory(data)
     let res = checkSum(data)
     console.timeEnd('main')
     return res
@@ -32,22 +33,65 @@ function shiftMemory(object) {
     console.time('shiftMemory')
     let array = object.data
     let length = array.length - 1;
-    let j = 0
-    for (let i = length; i >= 0; i--) {
-        let fileId = array[i]
-        if ( fileId === '.') {
-            continue;
-        };
-        let size = object.inventory[fileId]
-
-        //findContigousFamily(array, size)
-        i -= Number(size);
-        console.log('file: ', fileId,' size: ', fileId < 10 ? ` ${size}` : size)
+    let inventory = object.inventory;
+    for (let i = length; i >= 0; i) {
+        if (array[i] !== '.') {
+            let size = inventory[array[i]]
+            console.log('size: ',size)
+            let memorySlot = findContigousMemory(array, size);
+            if (memorySlot !== undefined) {
+                console.log('memory slot: ', memorySlot)
+                console.log('move block')
+                moveBlock(array,i, memorySlot, size)
+            }
+            i -= size;
+        } else i -= 1;
     }
     console.timeEnd('shiftMemory')
+    console.log(array)
     return array;
 }
 
+function moveBlock(array, from, to, length) {
+    let min = from - length;
+    for (let i = from; i > min; i--){
+        console.log('move single')
+        moveSingle(array, i, to);
+        to +=1;
+    }
+}
+
+function moveSingle(array, from, to) {
+    let temp = array[to];
+    array[to] = array[from]
+    array[from] = temp
+}
+function findContigousMemory(array, size) {
+    let i = 0;
+    let start;
+    let length = 0;
+    // find first empty space '.'
+    while (i < array.length) {
+        if (array[i] === '.'){
+            start = i;
+            console.log('i:', i, 'start:', start, 'size:', size)
+            while (array[i] === '.') {
+                console.log('i',i)
+                length += 1;
+                i++;
+                if (length === Number(size)) {
+                    console.log('length: ', length)
+                    return start;
+                }
+            }
+        }
+        length = 0;
+        i++;
+    }
+    return undefined; 
+    // count number of contigous '.'s
+    // if count === size return start index
+}
 
 function transformData(arr) {
     console.time('transformData')
@@ -71,7 +115,35 @@ function transformData(arr) {
     console.timeEnd('transformData')
     return { data:dataArray, inventory }
 }; 
-main(data)
+let obj = transformData(sample)
+
+
+
+console.log('data: \n---\n')
+console.table(obj.data)
+console.log('inventory:\n---\n',obj.inventory)
+traverse(obj);
+shiftMemory(obj);
+function traverse(obj){
+    let arr = obj.data;
+    let length = arr.length - 1;
+    let inv = obj.inventory;
+    for (let i = length; i >=0; i) {
+        if (arr[i] !== '.') {
+            size = inv[arr[i]];
+            let j = i;
+            let cap = j - size;
+            for (j; j > cap; j--) {
+            }
+            i -= size
+        } else i -= 1;
+    }
+}
+
+
+
+
+//main(sample)
 
 function writeFile(path, data) {
     fs.writeFile(path, data.toString(), err => {
